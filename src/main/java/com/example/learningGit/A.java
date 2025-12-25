5ef377c68af34805d4454761c07170f5b8e79578
Vaibhav Rawade <vaibhavrawade123@gmail.com>
2025-12-25 00:36:39 -1200

    done-main

--- src/main/java/com/example/learningGit/A.java
+++ src/main/java/com/example/learningGit/A.java
@@ -2,7 +2,9 @@ package com.example.learningGit;
 
 public class A 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("added new code");
+		System.out.println("inside main branch");
 	}
 }
